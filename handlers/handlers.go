a0a8ee9d6e9e4de1232509d68f354d82c4556920
Jirawat <jirawat@entercorp.net>
2019-01-21 19:07:46 +0700

    graceful shutdown

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -21,7 +21,7 @@ func Router(buildTime, commit, release string) *mux.Router {
 
 	r := mux.NewRouter()
 	r.HandleFunc("/home", home(buildTime, commit, release)).Methods("GET")
-	// r.HandleFunc("/healthz", healthz)
-	// r.HandleFunc("/readyz", readyz(isReady))
+	r.HandleFunc("/healthz", healthz)
+	r.HandleFunc("/readyz", readyz(isReady))
 	return r
 }
